127548a8422df20b4434c822758f7d2fed0fd920
3920038a3 <kyhienlanh@gmail.com>
2024-10-13 00:44:46 +0700

    first commit

---
+++ app/src/main/java/com/example/firebase/Item.kt
@@ -0,0 +1,5 @@
+package com.example.firebase
+
+class Item (var id:String,var name:String,var price:Double,var description:String){
+    //Mỗi sản phẩm sẽ có các trường như: id, name, price, description.
+}
